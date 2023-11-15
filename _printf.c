3664f39bd48bfcf2cd2b51d8f50a27cf85f0a3c2
gacenga <jimwillymurigu@gmail.com>
2023-11-15 00:24:59 -0800

    correction

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdarg.h>
 /**
-* printf - produces output according to a format
+* _printf - produces output according to a format
 *
 * @format:format
 * @...:arguments
@@ -9,46 +9,44 @@
 */
 int _printf(const char *format, ...)
 {
-  va_list args;
-  int i = 0;
-  int j = 0;
-  char *ptr;
-  va_start(args, format);
-  if (format == NULL)
-    return(-1);
-  while (*format)
-    {
-      if (format != '%')
-	{
-	  _putchar(*format);
-	  j++;
-	}
-      else
-	{
-	  format++;
-  if (*format == '%')
-    {
-      print_37(void);
-      j++;
-    }
-  else if (*format == 'c')
-    {
-      print_c(va_list args);
-      j++;
-    }
-  else if (*format == 's')
-    {
-      str = va_arg(args, char*);
-      while(*str != '\0')
-	{
-	  i++;
-	}
-      j = i;
-      print_s(va_list args);
-    }
-	}
-      format++;
-    }
-  va_end(args);
-  return(j);
+va_list args;
+int i = 0;
+int j = 0;
+char *ptr;
+va_start(args, format);
+if (format == NULL)
+return (-1);
+while (*format)
+{
+if (format != '%')
+{
+_putchar(*format);
+j++;
+}
+else
+{
+format++;
+if (*format == '%')
+{
+print_37(void);
+j++;
+}
+else if (*format == 'c')
+{
+print_c(va_list args);
+j++;
+}
+else if (*format == 's')
+{
+str = va_arg(args, char*);
+for (i = 0; *str != '\0'; i++)
+;
+j = i;
+print_s(va_list args);
+}
+}
+format++;
+}
+va_end(args);
+return (j);
 }
