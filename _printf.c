74ac017b546b2951f18b5e852bb7cd9d7cb488fc
gacenga <jimwillymurigu@gmail.com>
2023-11-15 00:14:21 -0800

    printf function

---
+++ _printf.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+* printf - produces output according to a format
+*
+* @format:format
+* @...:arguments
+* Return:number of characters printed
+*/
+int _printf(const char *format, ...)
+{
+  va_list args;
+  int i = 0;
+  int j = 0;
+  char *ptr;
+  va_start(args, format);
+  if (format == NULL)
+    return(-1);
+  while (*format)
+    {
+      if (format != '%')
+	{
+	  _putchar(*format);
+	  j++;
+	}
+      else
+	{
+	  format++;
+  if (*format == '%')
+    {
+      print_37(void);
+      j++;
+    }
+  else if (*format == 'c')
+    {
+      print_c(va_list args);
+      j++;
+    }
+  else if (*format == 's')
+    {
+      str = va_arg(args, char*);
+      while(*str != '\0')
+	{
+	  i++;
+	}
+      j = i;
+      print_s(va_list args);
+    }
+	}
+      format++;
+    }
+  va_end(args);
+  return(j);
+}
--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
+int _printf(const char *format, ...);
 int _putchar(char c);
 int _strlen(char *str);
 int _strlenc(const char *str);
