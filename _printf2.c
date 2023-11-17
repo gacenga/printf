0a968ef62e4ed265756c7db2115024cb43f13f96
gacenga <jimwillymurigu@gmail.com>
2023-11-17 06:59:20 -0800

    correction

--- _printf2.c
+++ _printf2.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdarg.h>
-#include <stddef.h>
 /**
 * _printf - produces output according to a format
 *
@@ -13,32 +12,27 @@ int _printf2(const char *format, ...)
 va_list args;
 int j = 0;
 va_start(args, format);
-if (format == NULL)
-return (-1);
-while (*format)
-{
-if (*format != '%')
-{
-_putchar(*format);
-j++;
-}
-else
-{
-format++;
-if(*format == 'd' || *format == 'i')
-{
-int num = va_arg(args, int);
-if (num < 0)
-{
-_putchar('-');
-j++;
-num = -num;
-}
-j += print_number(num);
-}
-}
-format++;
-}
+if (format != NULL) {
+        while (*format) {
+            if (*format != '%') {
+                _putchar(*format);
+                j++;
+            } else {
+                format++;
+                if (*format == 'd' || *format == 'i') {
+                    int num = va_arg(args, int);
+                    if (num < 0) {
+                        _putchar('-');
+                        j++;
+                        num = -num;
+                    }
+                    j += print_number(num);
+                }
+            }
+            format++;
+        }
+    }
+
 va_end(args);
 return (j);
 }
